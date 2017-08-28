31f0d4ff41eae42222f621ae43cce31edacd00e6
Kevin <hihellohi610@gmail.com>
2017-08-28 17:17:37 +1000

    prototypes

--- EuclideanVector.h
+++ EuclideanVector.h
@@ -1,14 +1,30 @@
 #ifndef EVEC_H
 #define EVEC_H
 
+#include <vector>
+#include <list>
+#include <initializer_list>
+
 namespace evec{
 	class EuclideanVector{
 		private:
 			
 		public:
-			EuclideanVector(unsigned int dimensions);
-			EuclideanVector(unsigned int dimensions, double magnitude);
-			EuclideanVector(
+			EuclideanVector(unsigned int);
+			EuclideanVector(unsigned int, double);
+			EuclideanVector(const std::vector<double>::iterator&, const std::vector<double>::iterator&);
+			EuclideanVector(const std::list<double>::iterator&, const std::list<double>::iterator&);
+			EuclideanVector(const std::initializer_list<double>&); 
+			EuclideanVector(const EuclidianVector&); 
+			EuclideanVector(const EuclidianVector&&); 
+			~EuclidianVector();
+
+			void operator=(const EuclidianVector&);
+			void operator=(const EuclidianVector&&);
+			unsigned int getNumDimensions() const;
+			double get(unsigned int) const;
+			double getEuclideanNorm() const;
+
 	};
 } //evec
 
