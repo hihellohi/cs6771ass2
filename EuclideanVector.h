4ba81e6fc5dd86e166ea006d0b8b706c1dfd4b4e
Kevin <hihellohi610@gmail.com>
2017-08-28 15:49:23 +1000

    initial commit

---
+++ EuclideanVector.h
@@ -0,0 +1,15 @@
+#ifndef EVEC_H
+#define EVEC_H
+
+namespace evec{
+	class EuclideanVector{
+		private:
+			
+		public:
+			EuclideanVector(unsigned int dimensions);
+			EuclideanVector(unsigned int dimensions, double magnitude);
+			EuclideanVector(
+	};
+} //evec
+
+#endif
